ebf2fd839d7b90d971c5d1d09f54dac7164330a5
Leonidas51 <kirpichik61@gmail.com>
2019-03-05 16:02:49 +0300

    hello world

---
+++ ogolodali/__init__.py
@@ -0,0 +1,27 @@
+import os
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask import Flask
+from flask_pymongo import PyMongo
+
+class JSONEncoder(json.JSONEncoder):
+	''' extend json-encoder class'''
+
+	def default(self, o):
+		if isinstance(o, ObjectId):
+			return str(o)
+		if isinstance(o, datetime.datetime):
+			return str(o)
+		return json.JSONEncoder.default(self, o)
+
+ogolodali = Flask(__name__)
+
+# add mongo url to flask config, so that flask_pymongo can use it to make connection
+ogolodali.config['MONGO_URI'] = os.environ.get('DB')
+mongo = PyMongo(ogolodali)
+
+# use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+ogolodali.json_encoder = JSONEncoder
+
+from ogolodali.controllers import *
