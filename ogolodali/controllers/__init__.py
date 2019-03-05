e4c7a0fd42385ce2fd1ca265234d4fff4cb11e98
Leonidas51 <kirpichik61@gmail.com>
2019-03-05 21:26:51 +0300

    recipes controller out

--- ogolodali/controllers/__init__.py
+++ ogolodali/controllers/__init__.py
@@ -1,13 +1,1 @@
-import os
-from flask import request, jsonify
-from ogolodali import ogolodali, mongo
-import logger
-
-ROOT_PATH = os.environ.get('ROOT_PATH')
-LOG = logger.get_root_logger(
-__name__, filename=os.path.join(ROOT_PATH, 'output.log'))
-
-@ogolodali.route('/recipe/<recipe_id>/', methods=['GET'])
-def recipe(recipe_id):
-	data = mongo.db.recipes.find_one({u'numeric_id': int(recipe_id)})
-	return jsonify(data), 200
+from .recipes import *
