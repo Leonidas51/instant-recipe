06c770843894a63e0f3e8e8105e04bf90b199513
Leonidas51 <kirpichik61@gmail.com>
2019-03-22 14:48:53 +0300

    infinite scroll also ads????

--- ogolodali/frontend/src/pages/RecipeList.js
+++ ogolodali/frontend/src/pages/RecipeList.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import InfiniteScroll from 'react-infinite-scroller';
 import SearchArea from "../components/SearchArea";
 import Recipe from "../components/Recipe";
 import "./RecipeList.css";
@@ -13,11 +14,13 @@ class RecipeList extends React.Component {
     this.state = {error: null,
                   is_loaded: false,
                   recipe_list: [],
-                  recipes_shown: ITEMS_ON_PAGE,
+                  shown_recipes: [],
+                  recipes_on_page: ITEMS_ON_PAGE,
+                  has_more: false,
                   selected_ings: URL_data['selected_ings'],
                   selected_ings_ids: URL_data['selected_ings_ids']};
 
-    this.onLoadMoreClick = this.onLoadMoreClick.bind(this);
+    this.loadMore = this.loadMore.bind(this);
   }
 
   getIngsFromURL(URL_string) {
@@ -40,6 +43,8 @@ class RecipeList extends React.Component {
     if(prevProps.match.params.search !== this.props.match.params.search) {
       this.setState({
         is_loaded: false,
+        shown_recipes: [],
+        has_more: false,
         selected_ings: URL_data['selected_ings'],
         selected_ings_ids: URL_data['selected_ings_ids']
       }, () => {
@@ -59,6 +64,7 @@ class RecipeList extends React.Component {
             (result) => {
               this.setState({
                 is_loaded: true,
+                has_more: true,
                 recipe_list: result
               });
             },
@@ -78,38 +84,64 @@ class RecipeList extends React.Component {
     });
   }
 
-  /* dom events */
+  loadMore() {
+    console.log('need more');
+    let recipes = this.state.shown_recipes;
 
-  onLoadMoreClick() {
-    const ITEMS_TO_LOAD = 10;
-
-    this.setState((prevState) => {
-      return {recipes_shown: prevState.recipes_shown + ITEMS_TO_LOAD};
+    this.state.recipe_list
+      .slice(recipes.length, recipes.length + this.state.recipes_on_page)
+      .map(recipe => {
+        recipes.push(recipe);
     })
+
+    console.log(recipes.length, this.state.recipe_list.length);
+    if(recipes.length >= this.state.recipe_list.length) {
+      this.setState({has_more: false});
+    }
+
+    this.setState({shown_recipes: recipes});
   }
+
+  /* dom events */
 
   /* end dom events */
 
   render() {
     const { error, is_loaded, recipe_list, selected_ings } = this.state;
+
     return(
       <div className="content-area_recipe-list">
         <SearchArea
           showSample={false}
-          preselectedIngs={this.state.selected_ings}
+          preselectedIngs={selected_ings}
         />
 
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadMore}
+          hasMore={this.state.has_more}
+        >
+
         {
-          this.state.recipe_list.slice(0, this.state.recipes_shown).map(recipe => {
+          this.state.shown_recipes.map((recipe, i) => {
             return (
               <div key={recipe._id}>
                 <Recipe rec={recipe} />
                 <hr />
+                {
+                  (i+1) % 10 === 0 ? (
+                  <div>
+                    <div>hi i'm an ad</div>
+                    <hr />
+                  </div>
+                  ) : null
+                }
               </div>
             )
           })
         }
-        <div onClick={this.onLoadMoreClick}>Load More</div>
+
+        </InfiniteScroll>
       </div>
     )
   }
