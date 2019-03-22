99b2f2d85abad1e6cd7a281d70570485aaf66882
Pauline <Pauline-sh-hub@yandex.ru>
2019-03-22 18:58:55 +0300

    api for getting ingredient by id(s)

--- ogolodali/controllers/ingredients.py
+++ ogolodali/controllers/ingredients.py
@@ -1,5 +1,6 @@
 import os
 import re
+from bson.objectid import ObjectId
 from flask import request, jsonify, Blueprint
 from ogolodali import mongo
 import logger
@@ -28,10 +29,27 @@ def read_random_ingredient():
     if request.method == 'GET':
         try:
             data = mongo.db.ingredients.aggregate([{ '$sample': {'size': 1} }])
-            if data == None:
+            data = list(data)
+            if data == None or len(data) == 0:
                 return jsonify(data = 'Nothing was found!'), 204
-            data = list(data)[0]
+            data = data[0]
             return jsonify(data), 200
         except Exception as e:
             LOG.error('error while trying to read_random_ingredient: ' + str(e))
+            return jsonify(data = 'Nothing was found!'), 204
+
+@ingredients_bp.route('/ingredient_by_id/<string:ingredients_list>/', methods=['GET'])
+def read_ingredient_by_id_list(ingredients_list):
+    if request.method == 'GET':
+        try:
+            ingredients_list = ingredients_list.split('&')
+            ingredients_list = [ObjectId(ingredient) for ingredient in ingredients_list]
+            data = mongo.db.ingredients.find({'_id': {'$in': ingredients_list}})
+            LOG.info(str(ingredients_list))
+            data = list(data)
+            if data == None or len(data) == 0:
+                return jsonify(data = 'Nothing was found!'), 204
+            return jsonify(data), 200
+        except Exception as e:
+            LOG.error('error while trying to read_ingredient_by_id_list: ' + str(e))
             return jsonify(data = 'Nothing was found!'), 204
