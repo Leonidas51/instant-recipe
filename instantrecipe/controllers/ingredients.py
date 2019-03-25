960c82bf5e5775bc4cf42db64fa0ffbf6c58d922
Pauline <Pauline-sh-hub@yandex.ru>
2019-03-25 15:20:51 +0300

    restoring order of ingredients after reload

--- instantrecipe/controllers/ingredients.py
+++ instantrecipe/controllers/ingredients.py
@@ -38,6 +38,18 @@ def read_random_ingredient():
             LOG.error('error while trying to read_random_ingredient: ' + str(e))
             return jsonify(data = 'Nothing was found!'), 204
 
+def restore_ings_order(data, ingredients_list):
+    data_ids = []
+    for data_val in data:
+        data_ids.append(data_val['_id'])
+    for i, val in enumerate(ingredients_list):
+        if ingredients_list[i] != data[i]['_id']:
+            buf = data[i]
+            index = data_ids.index(ingredients_list[i])
+            data[i] = data[index]
+            data[index] = buf
+    return data
+
 @ingredients_bp.route('/ingredient_by_id/<string:ingredients_list>/', methods=['GET'])
 def read_ingredient_by_id_list(ingredients_list):
     if request.method == 'GET':
@@ -45,8 +57,8 @@ def read_ingredient_by_id_list(ingredients_list):
             ingredients_list = ingredients_list.split('&')
             ingredients_list = [ObjectId(ingredient) for ingredient in ingredients_list]
             data = mongo.db.ingredients.find({'_id': {'$in': ingredients_list}})
-            LOG.info(str(ingredients_list))
             data = list(data)
+            data = restore_ings_order(data, ingredients_list)
             if data == None or len(data) == 0:
                 return jsonify(data = 'Nothing was found!'), 204
             return jsonify(data), 200
