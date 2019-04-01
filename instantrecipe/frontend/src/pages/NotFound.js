f5231983b485b63185943f6e4461eda28daef6d8
Pauline <Pauline-sh-hub@yandex.ru>
2019-04-01 15:49:46 +0300

    error pages (404, 500)

--- instantrecipe/frontend/src/pages/NotFound.js
+++ instantrecipe/frontend/src/pages/NotFound.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./NotFound.css";
 
 class NotFound extends React.Component {
   constructor(props) {
@@ -7,7 +8,11 @@ class NotFound extends React.Component {
   }
 
   render() {
-    return <div>Not Found!</div>
+    return (
+    <div className="content-area_not-found ">
+      <h2 className="error__title">404</h2>
+      <p className="error__description">Страница не найдена!</p>
+    </div>);
   }
 }
 
