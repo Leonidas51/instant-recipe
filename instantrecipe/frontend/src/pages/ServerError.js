238872d7aea00ed702164dea432ae59c88732695
Pauline <Pauline-sh-hub@yandex.ru>
2019-04-01 15:50:17 +0300

    error page (500) files

---
+++ instantrecipe/frontend/src/pages/ServerError.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./ServerError.css";
+
+class ServerError extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div className="content-area_not-found ">
+      <h2 className="error__title">500</h2>
+      <p className="error__description">Ошибка на сервере!</p>
+    </div>);
+  }
+}
+
+export default ServerError;
