9a5c22cc087492d5fa5ac2bd508787b96c2b205c
Pauline <Pauline-sh-hub@yandex.ru>
2019-04-25 22:55:54 +0300

    oopsie

--- instantrecipe/controllers/recipes.py
+++ instantrecipe/controllers/recipes.py
@@ -65,8 +65,8 @@ def remove_salt():
 			new_ingredient_names.pop(salt_id, None)
 			result = mongo.db.recipes.update_one(
 				{'_id': recipe['_id']},
-				{'$set': {'ingredient_names.mandatory': new_ingredient_names}})	
-	result = mongo.db.recipes.delete_one({'_id': ObjectId(salt_id)})
+				{'$set': {'ingredient_names.mandatory': new_ingredient_names}})
+	result = mongo.db.recipes.delete_one({'_id': salt_id})
 
 	return jsonify(data = 'removed salt'), 200
 
